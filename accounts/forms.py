2c6e3f67df9525ddc9bfa729e20ba88dd97066c0
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-07-08 20:55:12 -0400

    Login/Logout System

---
+++ accounts/forms.py
@@ -0,0 +1,27 @@
+from django import forms  
+from django.contrib.auth.models import User  
+from django.contrib.auth.forms import UserCreationForm  
+from django.core.exceptions import ValidationError  
+from django.forms.forms import Form  
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label="Username")
+    email = forms.EmailField(label="Email Address", required=True)
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already exists.")
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Username already exists.")
+        return username
---
+++ accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to a home page or dashboard
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'accounts/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been created! You are now able to log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'register_form': form})
